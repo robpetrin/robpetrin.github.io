0959aa72536e4a4e715461618df378daaceb9085
Rob <robpetrin@gmail.com>
2021-01-24 23:12:29 -0500

    2021, bay b!

--- script.js
+++ script.js
@@ -1,58 +1,67 @@
-window.onscroll = function() {myFunction()};
+var navItems = document.querySelectorAll('.nav-item')
+var socialItems = document.querySelectorAll('.social-item')
+var resume = document.getElementById('resume')
+var stackIcons = document.querySelectorAll('.icon-row-stack > i')
+var form = document.getElementById('form')
+var formURL = 'https://script.google.com/macros/library/d/19GkEdQnq9s_1XPkyVF8E2MQEUhJdI6QlGSM4gf69mMKXZAWPD4_fqx4b/1'
+var formSubmit = document.getElementById('form-submit')
+var formStatus = document.getElementById('form-status')
 
-function myFunction() {
-	const resumeButton = document.getElementById("resume-button")
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    resumeButton.classList.add("btn-light");
-		resumeButton.classList.remove("btn-outline-light");
-  } else {
-    resumeButton.classList.add("btn-outline-light");
-		resumeButton.classList.remove("btn-light");
-  }
+// Clears active tab when switching tabs, used by both below functions
+function hideActive() {
+	var active = document.querySelector('.active')
+	var activeName = active.getAttribute('val')
+	var activeSection = document.getElementById(activeName)
+	activeSection.style.display = 'none'
+	active.classList.remove('active')
 }
 
-  window.addEventListener("DOMContentLoaded", function() {
+// Hide active tab, activate a new one
+navItems.forEach(item => {
+	item.addEventListener('click', function() {
+		hideActive()
+		var clicked = this.getAttribute('val')
+		var show = document.getElementById(clicked)
+		this.classList.add('active')
+		show.style.display = 'block'
+	}
+)})
 
-    // get the form elements defined in your form HTML above
-    
-    var form = document.getElementById("my-form");
-    var button = document.getElementById("my-form-button");
-    var status = document.getElementById("my-form-status");
+// A slightly different version of the above for the hidden email tab
+function showEmail() {
+	hideActive()
+	var email = document.getElementById('email')
+	var emailIcon = document.getElementById('email-icon')
+	email.style.display = 'block'
+	emailIcon.classList.add('active')
+	emailIcon.focus()
+}
 
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      button.style = "display: none ";
-      status.innerHTML = "Thanks! I'll be in touch soon!";
-    }
+// Show social icon label tooltip text when hovering on them
+socialItems.forEach(item => {
+	var resume = document.getElementById('resume')
+	item.addEventListener('mouseover', function() {
+		var label = this.getAttribute('name')
+		resume.innerText = label
+	})
+	item.addEventListener('mouseout', function() {
+		resume.innerText = "Rob's Résumé"
+	})
+})
 
-    function error() {
-      status.innerHTML = "Oops! There was a problem.";
-    }
+// Show tooltip when hovering on icons in Dev tab
+stackIcons.forEach(item => {
+	item.addEventListener('mouseover', function() {
+		item.classList.add('show-icon')
+	})	
+	item.addEventListener('mouseout', function() {
+		item.classList.remove('show-icon')
+	})	
+})
 
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
+form.addEventListener('submit',function(e) {
+  e.preventDefault()
+	form.reset()
+	formSubmit.style.display = 'none'
+	formStatus.innerHTML = 'Thanks! I\'ll be in touch soon!'
+	})
