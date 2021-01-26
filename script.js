d1ab20cc65c2fe078a8c87440a23bcc7535f99a2
Rob <robpetrin@gmail.com>
2021-01-25 20:54:33 -0500

    Fixing my dumb form

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ socialItems.forEach(item => {
 		resume.innerText = label
 	})
 	item.addEventListener('mouseout', function() {
-		resume.innerText = "Rob's Résumé"
+		resume.innerText = 'Rob\'s Résumé'
 	})
 })
 
@@ -59,9 +59,41 @@ stackIcons.forEach(item => {
 	})	
 })
 
-form.addEventListener('submit',function(e) {
-  e.preventDefault()
-	form.reset()
-	formSubmit.style.display = 'none'
-	formStatus.innerHTML = 'Thanks! I\'ll be in touch soon!'
-	})
+window.addEventListener('DOMContentLoaded', function() {
+	// Success and Error functions for after the form is submitted
+	
+	function success() {
+		form.reset();
+		formSubmit.style = 'display: none ';
+		formStatus.innerHTML = 'Thanks! I\'ll be in touch soon!';
+	}
+
+	function error() {
+		formStatus.innerHTML = 'Oops! There was a problem.';
+	}
+
+	// handle the form submission event
+
+	form.addEventListener('submit', function(ev) {
+		ev.preventDefault();
+		var data = new FormData(form);
+		ajax(form.method, form.action, data, success, error);
+	});
+	});
+	
+	// helper function for sending an AJAX request
+
+	function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader('Accept', 'application/json');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState !== XMLHttpRequest.DONE) return;
+		if (xhr.status === 200) {
+		success(xhr.response, xhr.responseType);
+		} else {
+		error(xhr.status, xhr.response, xhr.responseType);
+		}
+	};
+	xhr.send(data);
+	}
